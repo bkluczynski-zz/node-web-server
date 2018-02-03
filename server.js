beee7dc1a33ee134d64656825cec12678905d943
Bart <bart@paybase.io>
2018-02-03 18:33:34 +0000

    made a port dynamic for heroku

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const path = require('path');
 
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('view engine', hbs);
 app.use(express.static(path.join(__dirname, '/public')));
-hbs.registerHelper('getCurrentYear', () =>
-  new Date().getFullYear());
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('getCapital', text => text.toUpperCase());
 
 app.get('/', (req, res) => {
-  // res.send('Hello Express');
   res.render('home.hbs', {
     pageTitle: 'Home page',
     welcomeMessage: 'Welcome to my website',
@@ -26,13 +26,12 @@ app.get('/about', (req, res) => {
     currentYear: new Date().getFullYear(),
   });
 });
-
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Wrong endpoint typed',
   });
 });
 
-app.listen(3000, () => {
-  console.log('server is up on the port 3000');
+app.listen(port, () => {
+  console.log(`server is up on the port ${port}`);
 });
